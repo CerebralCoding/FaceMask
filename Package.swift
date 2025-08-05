4676591826b9b0254ab23221e6b8ddcf11d02573
Sebastian Christiansen <mail@cerebralcoding.com>
2025-08-05 09:42:49 +0200

    Ran formatter

--- Package.swift
+++ Package.swift
@@ -16,11 +16,11 @@ let package = Package(
         ),
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-            .executableTarget(
-                name: "FaceMask",
-                dependencies: ["notcurses"],
-                cxxSettings: [.define("ACCELERATE_NEW_LAPACK", to: "1"), .define("ACCELERATE_LAPACK_ILP64", to: "1")],
-                linkerSettings: [.linkedFramework("Accelerate")],
-            ),
-        ],
+        .executableTarget(
+            name: "FaceMask",
+            dependencies: ["notcurses"],
+            cxxSettings: [.define("ACCELERATE_NEW_LAPACK", to: "1"), .define("ACCELERATE_LAPACK_ILP64", to: "1")],
+            linkerSettings: [.linkedFramework("Accelerate")],
+        ),
+    ],
 )
